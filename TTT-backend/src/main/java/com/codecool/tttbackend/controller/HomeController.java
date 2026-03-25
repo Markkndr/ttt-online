5a017435a79777f32e6f807920e44f4be21d7d88
Kondor Mark <kondormarkk@gmail.com>
2026-03-25 13:41:01 +0100

    home controller added

---
+++ TTT-backend/src/main/java/com/codecool/tttbackend/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.codecool.tttbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public Map<String, String> home() {
+        return Map.of("status", "ok", "message", "TTT backend is running");
+    }
+}
