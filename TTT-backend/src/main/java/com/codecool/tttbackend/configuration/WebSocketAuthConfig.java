95f4c700e043118660788febcc6fba40717e4b8b
Kondor Mark <kondormarkk@gmail.com>
2026-03-25 21:16:02 +0100

    websocketAuth bugfix

--- TTT-backend/src/main/java/com/codecool/tttbackend/configuration/WebSocketAuthConfig.java
+++ TTT-backend/src/main/java/com/codecool/tttbackend/configuration/WebSocketAuthConfig.java
@@ -8,10 +8,14 @@ import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,28 +34,23 @@ public class WebSocketAuthConfig implements WebSocketMessageBrokerConfigurer {
         registration.interceptors(new ChannelInterceptor() {
             @Override
             public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
-                StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+                if (accessor == null) {
+                    return message;
+                }
 
                 if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-
-                    String authHeader =
-                            accessor.getFirstNativeHeader("Authorization");
-
-                    // >>> DEBUG LINE <<<
+                    String authHeader = accessor.getFirstNativeHeader("Authorization");
                     System.out.println("WS CONNECT auth header = " + authHeader);
 
                     if (authHeader != null && authHeader.startsWith("Bearer ")) {
-
                         String token = authHeader.substring(7);
 
                         if (jwtUtil.validateToken(token, false)) {
-
-                            String username =
-                                    jwtUtil.getUsernameFromToken(token, false);
-
-                            String rolesString =
-                                    jwtUtil.getRolesFromToken(token);
+                            String username = jwtUtil.getUsernameFromToken(token, false);
+                            String rolesString = jwtUtil.getRolesFromToken(token);
 
                             List<SimpleGrantedAuthority> authorities =
                                     Arrays.stream(rolesString.split(","))
@@ -62,17 +61,20 @@ public class WebSocketAuthConfig implements WebSocketMessageBrokerConfigurer {
                                             .collect(Collectors.toList());
 
                             var authentication =
-                                    new UsernamePasswordAuthenticationToken(
-                                            username,
-                                            null,
-                                            authorities
-                                    );
+                                    new UsernamePasswordAuthenticationToken(username, null, authorities);
 
                             accessor.setUser(authentication);
-
-                            // optional extra debug
                             System.out.println("WS user set = " + username);
                         }
+                    }
+                } else {
+                    Principal user = accessor.getUser();
+                    System.out.println("WS inbound command = " + accessor.getCommand() + ", user = " + user);
+
+                    if (user instanceof UsernamePasswordAuthenticationToken authentication) {
+                        SecurityContext context = SecurityContextHolder.createEmptyContext();
+                        context.setAuthentication(authentication);
+                        SecurityContextHolder.setContext(context);
                     }
                 }
 
